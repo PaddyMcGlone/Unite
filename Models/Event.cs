0ad20d9c1517e656fe614ecb8e47ad8f9ac29a8a
Paddy McGlone <paddymcglone@gmail.com>
2020-05-02 17:03:38 +0100

    Adding the base event model.

---
+++ Models/Event.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Unite.Models
+{
+    class Event
+    {
+        public Guid EntityId { get; set; }
+
+        public EventType Type { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
---
+++ Models/EventType.cs
@@ -0,0 +1,12 @@
+namespace Unite.Models
+{
+    // Represents the type of events within Unite    
+    public enum EventType
+    {
+        Sport = 1,
+
+        Social = 2,
+
+        Formal = 3
+    }
+}
